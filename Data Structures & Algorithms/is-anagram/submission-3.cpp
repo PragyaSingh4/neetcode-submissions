d024b9cd734de906c37b8e45de8f106e55acb31b
Pragya Singh <154442206+PragyaSingh4@users.noreply.github.com>
2026-04-17 22:27:13 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+        return false;
+
+        // unordered_map<char,int>mp;
+        // for(char ch:s)
+        // mp[ch]++;
+
+        // for(char ch:t)
+        // {
+        //     mp[ch]--;
+        //     if(mp[ch]<0)
+        //     return false;
+        // }
+        // return true;
+
+        int freq[26] = {0};
+        for(int i = 0;i<s.length();i++)
+        {
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+
+        for(int i = 0;i<26;i++)
+        {
+            if(freq[i]!=0)
+            return false;
+        }
+        return true;
+    }
+};
