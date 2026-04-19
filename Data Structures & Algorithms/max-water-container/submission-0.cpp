da4ed515f2d3d89b0d3aaf700c18ffcea3d992bf
Pragya Singh <154442206+PragyaSingh4@users.noreply.github.com>
2026-04-19 11:06:20 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int l = 0;
+        int r = heights.size();
+        int ans = INT_MIN;
+
+        while(l<r)
+        {
+            int area = (r-l)*min(heights[l],heights[r]);
+            ans = max(ans,area);
+
+            if(heights[l]<heights[r])
+            l++;
+            else
+            r--;
+        }
+        return ans;
+    }
+};
